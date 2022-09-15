a8e1f111bfc00ed3bc93b92669b5b5bb4ebcb53b
hellonayeon <hellonykwon@gmail.com>
2022-09-15 12:55:16 +0900

    add: Spring Cloud Gateway - Logging Filter

--- api-gateway-service/apigateway-service/src/main/java/me/hellonayeon/apigatewayservice/filter/LoggingFilter.java
+++ api-gateway-service/apigateway-service/src/main/java/me/hellonayeon/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package me.hellonayeon.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -11,34 +13,36 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 
-    public GlobalFilter() {
+    public LoggingFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return ((exchange, chain) -> {
 
+        GatewayFilter filter = new OrderedGatewayFilter(((exchange, chain) -> {
             // custom pre filter
             ServerHttpRequest req = exchange.getRequest();
             ServerHttpResponse resp = exchange.getResponse();
 
-            log.info("Global filter baseMessage: {}", config.getBaseMessage());
+            log.info("Logging filter baseMessage: {}", config.getBaseMessage());
 
             if (config.isPreLogger()) {
-                log.info("Global filter start: request id -> {}", req.getId());
+                log.info("Logging PRE filter: request id -> {}", req.getId());
             }
 
             // custom post filter
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
-                    log.info("Global filter end: response code -> {}", resp.getStatusCode());
+                    log.info("Logging POST filter: response code -> {}", resp.getStatusCode());
                 }
             }));
+        }), Ordered.LOWEST_PRECEDENCE);
 
-        });
+
+        return filter;
     }
 
     @Data
