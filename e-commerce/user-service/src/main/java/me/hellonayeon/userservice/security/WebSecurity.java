547129b70857275a1a3771cccf985ae770a932b4
hellonayeon <hellonykwon@gmail.com>
2022-09-16 16:48:07 +0900

    add: Users Microservice - AuthenticationFilter 추가, loadUserByUsername() 구현

--- e-commerce/user-service/src/main/java/me/hellonayeon/userservice/security/WebSecurity.java
+++ e-commerce/user-service/src/main/java/me/hellonayeon/userservice/security/WebSecurity.java
@@ -1,24 +1,54 @@
 package me.hellonayeon.userservice.security;
 
+import me.hellonayeon.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
+    private Environment env;
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.0.112")
+                .and()
+                .addFilter(new AuthenticationFilter());
 
         http.headers().frameOptions().disable();
 
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    
 }
--- e-commerce/user-service/src/main/java/me/hellonayeon/userservice/service/UserService.java
+++ e-commerce/user-service/src/main/java/me/hellonayeon/userservice/service/UserService.java
@@ -2,8 +2,9 @@ package me.hellonayeon.userservice.service;
 
 import me.hellonayeon.userservice.dto.UserDto;
 import me.hellonayeon.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
