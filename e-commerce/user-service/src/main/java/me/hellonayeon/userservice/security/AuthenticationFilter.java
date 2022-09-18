f424e0342bae0eb3f859ec3e3fbcb45d965fa77a
hellonayeon <hellonykwon@gmail.com>
2022-09-18 16:25:41 +0900

    fix: Spring Security 설정 변경 (WebSecurityConfigurerAdapter 상속)

--- e-commerce/user-service/src/main/java/me/hellonayeon/userservice/security/AuthenticationFilter.java
+++ e-commerce/user-service/src/main/java/me/hellonayeon/userservice/security/AuthenticationFilter.java
@@ -1,10 +1,18 @@
 package me.hellonayeon.userservice.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import me.hellonayeon.userservice.dto.UserDto;
+import me.hellonayeon.userservice.service.UserService;
 import me.hellonayeon.userservice.vo.RequestLogin;
+import org.bouncycastle.jcajce.BCFKSLoadStoreParameter.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -13,34 +21,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import org.springframework.stereotype.Component;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private UserService userService;
+    private Environment env;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager,
+        UserService userService,
+        Environment env) {
+        super.setAuthenticationManager(authenticationManager);
+        this.userService = userService;
+        this.env = env;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException {
-
+        HttpServletResponse response) throws AuthenticationException {
         try {
             RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
 
             return getAuthenticationManager().authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            creds.getEmail(),
-                            creds.getPassword(),
-                            new ArrayList<>()
-                    )
+                new UsernamePasswordAuthenticationToken(
+                    creds.getEmail(),
+                    creds.getPassword(),
+                    new ArrayList<>()
+                )
             );
-
-        } catch (IOException e) {
+        } catch(IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        HttpServletResponse response,
+        FilterChain chain,
+        Authentication authResult) throws IOException, ServletException {
+
     }
 }
--- e-commerce/user-service/src/main/java/me/hellonayeon/userservice/security/WebSecurity.java
+++ e-commerce/user-service/src/main/java/me/hellonayeon/userservice/security/WebSecurity.java
@@ -6,49 +6,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity {
-
-    private Environment env;
+public class WebSecurity extends WebSecurityConfigurerAdapter {
     private UserService userService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
 
-    @Autowired
     public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.env = env;
         this.userService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("192.168.0.112")
+
+        http.authorizeRequests().antMatchers("/users")
+                .hasIpAddress("192.168.0.5")
                 .and()
-                .addFilter(new AuthenticationFilter());
+                .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
-
-        return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter =
+            new AuthenticationFilter(authenticationManager(), userService, env);
+
+        return authenticationFilter;
     }
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
 }
